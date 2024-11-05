48f4653642d5401bd88864d48be911ad83e50a5c
rajendrakumar4302 <rajendrakumar4302@gmail.com>
2024-11-05 11:07:09 +0000

    Feat-Added Tabs and Blue Print of MobileFrame on Build Screen

---
+++ lib/component_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ComponentScreen extends StatelessWidget {
+  const ComponentScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Component Screen Content1'), // Replace with actual content
+    );
+  }
+}
